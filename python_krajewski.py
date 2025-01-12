fcd909cfdbda3f9b525b4f5d2547913888e1e630
gammaro85 <gammaro85@gmail.com>
2025-01-12 18:12:15 +0100

    new data in journaldata

--- python_krajewski.py
+++ python_krajewski.py
@@ -1,11 +1,25 @@
 import csv
+import requests
 
-def import_csv(file_path):
-    with open(file_path, mode='r') as file:
-        csv_reader = csv.reader(file)
-        data = [row for row in csv_reader]
+def import_csv_from_url(url):
+    response = requests.get(url)
+    response.raise_for_status()  # Check if the request was successful
+    content = response.content.decode('utf-8').splitlines()
+    csv_reader = csv.reader(content, delimiter=';')
+    data = [row for row in csv_reader]
     return data
 
-file_path = 'https://github.com/gammaro85/2.6assigmnment/blob/main/journaldata.csv'
-data = import_csv(file_path)
-print(data)
+url = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/journaldata.csv'
+
+# Import data from URL
+url_data = import_csv_from_url(url)
+print(url_data)
+
+# Prompt user to check journal title
+journal_title = input("Check journal title: ")
+for row in url_data:
+    if row[0] == journal_title:
+        print("Journal found:", row)
+        break
+else:
+    print("Journal not found.")
