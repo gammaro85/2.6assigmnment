352b8573b727e0443e08ea8e23ca9072b719423d
gammaro85 <gammaro85@gmail.com>
2025-01-12 19:51:20 +0100

    Add journal data CSV and update URLs in Python script

--- python_krajewski.py
+++ python_krajewski.py
@@ -18,8 +18,8 @@ def import_funds(url2):
     return data
 
 # Define URLs
-url1 = 'https://github.com/gammaro85/2.6assigmnment/blob/b8cf083dad02dbb8f018f2b9e6e655efd94a89ac/journaldata.csv'
-url2 = 'https://github.com/gammaro85/2.6assigmnment/blob/main/money.csv'
+url1 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/b8cf083dad02dbb8f018f2b9e6e655efd94a89ac/journaldata.csv'
+url2 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/refs/heads/main/money.csv'
 
 # Import data from URLs
 journal_data = import_journals(url1)
@@ -30,7 +30,7 @@ print(fund_data)
 # Prompt user to check journal title
 journal_title = input("Check journal title: ")
 for row in journal_data:
-    if row[0] == journal_title:
+    if row[0] = journal_title:
         print("Journal found")
         print(oxygenium)
         break
