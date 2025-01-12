8026e5f73f208f26de99d8fbf450b75c61d3ca4e
gammaro85 <gammaro85@gmail.com>
2025-01-12 19:01:39 +0100

    next step

--- python_krajewski.py
+++ python_krajewski.py
@@ -1,25 +1,37 @@
 import csv
 import requests
 
-def import_csv_from_url(url):
-    response = requests.get(url)
+def import_journals(url1):
+    response = requests.get(url1)
     response.raise_for_status()  # Check if the request was successful
     content = response.content.decode('utf-8').splitlines()
     csv_reader = csv.reader(content, delimiter=';')
     data = [row for row in csv_reader]
     return data
 
-url = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/journaldata.csv'
+def import_funds(url2):
+    response = requests.get(url2)
+    response.raise_for_status()  # Check if the request was successful
+    content = response.content.decode('utf-8').splitlines()
+    csv_reader = csv.reader(content, delimiter=';')
+    data = [row for row in csv_reader]
+    return data
 
-# Import data from URL
-url_data = import_csv_from_url(url)
-print(url_data)
+# Define URLs
+url1 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/journaldata.csv'
+url2 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/funddata.csv'
+
+# Import data from URLs
+journal_data = import_journals(url1)
+fund_data = import_funds(url2)
+print(journal_data, fund_data)
 
 # Prompt user to check journal title
 journal_title = input("Check journal title: ")
-for row in url_data:
+for row in journal_data:
     if row[0] == journal_title:
-        print("Journal found:", row)
+        print("Journal found")
+        print(oxygenium)
         break
 else:
     print("Journal not found.")
