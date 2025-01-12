f5f6d2154a285d8f5c96cd558a37abfae3f72e2f
gammaro85 <gammaro85@gmail.com>
2025-01-12 19:22:19 +0100

    next

--- python_krajewski.py
+++ python_krajewski.py
@@ -18,13 +18,14 @@ def import_funds(url2):
     return data
 
 # Define URLs
-url1 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/journaldata.csv'
-url2 = 'https://raw.githubusercontent.com/gammaro85/2.6assigmnment/main/funddata.csv'
+url1 = 'https://github.com/gammaro85/2.6assigmnment/blob/b8cf083dad02dbb8f018f2b9e6e655efd94a89ac/journaldata.csv'
+url2 = 'https://github.com/gammaro85/2.6assigmnment/blob/main/money.csv'
 
 # Import data from URLs
 journal_data = import_journals(url1)
 fund_data = import_funds(url2)
-print(journal_data, fund_data)
+print(journal_data)
+print(fund_data)
 
 # Prompt user to check journal title
 journal_title = input("Check journal title: ")
